99720d000a544793e0af8232708cad380e9c344c
VaibhavKorade28 <85725768+VaibhavKorade28@users.noreply.github.com>
2023-04-20 14:58:51 +0530

    Update db.sql

--- db.sql
+++ db.sql
@@ -10,11 +10,13 @@ create table  customer (
   phone varchar(20),
   username varchar(20),
   passwd varchar(20));
+  
  create table  Item (
   itemid int primary key auto_increment,
   itemname varchar(20),
   price double(9,2),
   qty int check(qty>0));
+  
 create table category(
 catid int primary key auto_increment,
 catname varchar(20));
@@ -46,40 +48,49 @@ create table restaurent(
 	owner varchar(20),
 	email varchar(20),
 	phoneno varchar(20));
+	
 create table location(
 	loc_id int primary key auto_increment,
 	loc_name varchar(20));
+	
 create table transaction(
 	trans_id  int primary key auto_increment,
 	amount double(9,2),
 	trans_date Date);
+	
 create table payment(
 	pay_id int primary key auto_increment,
 	pay_date Date,
 	 amount double(9,2),
 	pay_mode varchar(20),
 	status varchar(20));
+	
 create table delivery(
 	del_id int primary key auto_increment,
 	dest varchar(20),
 	del_part varchar(20));
- create table  cart_item (
-  cart_item_id int primary key auto_increment,
-  itemname varchar(20),
-  price double(9,2),
-  qty int check(qty>0));
-  create table cart(
+	
+
    
 
 
 
 
 -- Create table to store cart related Information
-
+ create table cart(
+  item_id int primary key auto_increment,
+   itemname varchar(20),
+  price double(9,2),
+  qty int check(qty>0));
 
 
 -- Create table to store Vendor Information
 
-
+create table agency(
+   ag_id int primary key auto_increment,
+   ag_name varchar(20),
+   email varchar(20),
+   username varchar(20),
+   passwd varchar(20));
 
 
